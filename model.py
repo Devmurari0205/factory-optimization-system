e3cbf20fbe2af41290fbe7c342cec44e4c2a6e4c
Harsh Devmurari <harshdevmurari08@gmail.com>
2026-05-25 21:38:20 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,18 +1,17 @@
 import pandas as pd
+import os
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestRegressor
 
 def load_and_train():
-    df = pd.read_csv("data.csv")
+    path = os.path.join(os.path.dirname(__file__), "data.csv")
+    df = pd.read_csv(path)
 
-    # Date conversion
     df['Order Date'] = pd.to_datetime(df['Order Date'])
     df['Ship Date'] = pd.to_datetime(df['Ship Date'])
 
-    # Create Lead Time
     df['Lead Time'] = (df['Ship Date'] - df['Order Date']).dt.days
 
-    # Encoding
     le_region = LabelEncoder()
     le_ship = LabelEncoder()
     le_product = LabelEncoder()
@@ -21,17 +20,14 @@ def load_and_train():
     df['Ship Mode_enc'] = le_ship.fit_transform(df['Ship Mode'])
     df['Product_enc'] = le_product.fit_transform(df['Product Name'])
 
-    # Features
     X = df[['Region_enc','Ship Mode_enc','Product_enc','Units','Cost']]
     y = df['Lead Time']
 
     model = RandomForestRegressor()
     model.fit(X, y)
 
-    encoders = {
+    return df, model, {
         "region": le_region,
         "ship": le_ship,
         "product": le_product
     }
-
-    return df, model, encoders
